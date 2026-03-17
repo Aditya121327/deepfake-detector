4558eecd4d07b63b9728d30d0aec63489d872ee8
Aditya121327 <adityababel27@gmail.com>
2026-03-17 11:00:50 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.title("Deepfake Detector")
+
+uploaded_file = st.file_uploader(
+    "Upload Image",
+    type=["jpg", "png", "jpeg"]
+)
+
+if uploaded_file:
+    st.image(uploaded_file, caption="Uploaded Image")
